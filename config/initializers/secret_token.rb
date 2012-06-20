9bd5965733ba91b982c85badfa4657bdb2fddf27
Lauren Voswinkel <lauren@pikimal.com>
2012-06-19 21:19:43 -0400

    It starts.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ankath::Application.config.secret_token = 'adee65ad645e08d0614338182b3892304994efd8fb4e85ae3f727f4d865db5e40bbfdc7fab34cb946634bb9da24ccfc3edf54cfad0ea2b032fa0d997e4af64b3'
